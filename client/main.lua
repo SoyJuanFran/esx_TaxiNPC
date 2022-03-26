52a25faef7b77581ce486751868b473f32f93cac
Soy_JuanFran <59895113+AdictosRP@users.noreply.github.com>
2022-03-26 18:27:36 +0100

    1.0

--- client/main.lua
+++ client/main.lua
@@ -15,12 +15,18 @@ ESX = nil
 local hash = -573920724
 local vehicleHash = -956048545
 local ped = nil
+local targetX = nil
+local targetY = nil
 local taxiBlip = false
 local globalTaxi = nil
+local vehicle = nil
 local customer = nil
 local onTour = false
 local driveFinish = nil
-local ipe = ("176.31.53.98:30123")
+local aparcado = false
+local fuera = false
+local llegar = false
+local heading = 1
 
 Citizen.CreateThread(function()
 	while ESX == nil do
@@ -33,25 +39,18 @@ end)
 RegisterNetEvent('esx_aiTaxi:callTaxi')
 AddEventHandler('esx_aiTaxi:callTaxi', function(coords)
 	TriggerEvent('taxi:getTaxi', function() end)
-	while GetCurrentServerEndpoint() ~= ipe do
-	Citizen.Wait(1000)
-		print("Script robado a AdictosRP")
-		print("Discord: JuanFran#8945")
-		print("Si te ha llegado este script, habla con él.")
-	end
 	local taxionline = 0
 
         ESX.TriggerServerCallback('taxi:Taxi', function(taxi)
             taxionline = taxi
 
-  if taxionline < 1 then 
+  if taxionline < 2 then 
 	if customer then
 		ESX.ShowHelpNotification('Ya hay un taxi en camino')
 	else
 		customer = coords
 		-- get best spawnpoint
-		playerPed = GetPlayerPed(-1)
-		myCoords = GetEntityCoords(playerPed)
+		myCoords = GetEntityCoords(GetPlayerPed(-1))
 		x, y, z = table.unpack(GetEntityCoords(GetPlayerPed(-1), true))
 		ESX.ShowHelpNotification('Estamos llamando a un Taxista. Paciencia')
 			local posX = x
@@ -65,19 +64,34 @@ AddEventHandler('esx_aiTaxi:callTaxi', function(coords)
 				posY = y + math.random(-200, 200) 
 
 				_,posZ = GetGroundZFor_3dCoord(posX+.0,posY+.0,z+1,1)
-				local carretera = IsPointOnRoad(posX, posY, posZ)
-				local seguro = GetSafeCoordForPed(posX, posY, posZ, true, 16)
-				local road = GetClosestRoad(posX, posY, posZ, 1000.0, 1, false)
 				
-
-				print(GetDistanceBetweenCoords(myCoords, posX, posY, posZ))
-				if GetDistanceBetweenCoords(myCoords, posX, posY, posZ) < 70 and GetDistanceBetweenCoords(myCoords, posX, posY, posZ) > 200 and carretera == 1 and seguro == 1 and road == 1 then
-					canSpawn = false
-					break
-				else
-					canSpawn = true
-					
+				local ret, coordsTemp, heading = GetClosestVehicleNodeWithHeading(posX, posY, posZ, 1, 3.0, 0)
+				local retval, coordsSide = GetPointOnRoadSide(coordsTemp.x, coordsTemp.y, coordsTemp.z)
+				posX = coordsSide.x
+				posY = coordsSide.y
+				posZ = coordsSide.z
+				--local carretera = IsPointOnRoad(posX, posY, posZ)
+				--local seguro = GetSafeCoordForPed(posX, posY, posZ, true, 16)
+				local agua = GetWaterHeightNoWaves(posX, posY, posZ)
+				--local road = GetClosestRoad(posX, posY, posZ, 1000.0, 1, false)
+				
+				
+				
+				while agua do
+				posX = posX - 2.0
+				
+				Wait(0)
 				end
+				--print(coordsSide, GetDistanceBetweenCoords(myCoords, coordsSide.x, coordsSide.y, coordsSide.z))
+				
+				cabeza = heading
+					if GetDistanceBetweenCoords(myCoords, coordsSide.x, coordsSide.y, coordsSide.z, true) < 75 and GetDistanceBetweenCoords(myCoords, coordsSide.x, coordsSide.y, coordsSide.z) > 250 then
+						canSpawn = false
+						break
+					else
+						canSpawn = true
+					end
+				
 			until canSpawn
 			
 		while not HasModelLoaded(hash) do
@@ -90,12 +104,22 @@ AddEventHandler('esx_aiTaxi:callTaxi', function(coords)
 		end
 		if ped == nil then
 			ped =  CreatePed(4, hash, posX, posY, posZ + 2, 0.0, true, true)
+			
+			SetEntityCanBeDamaged(ped, false)
+			SetPedCanBeTargetted(ped, false)
+			SetDriverAbility(ped, 1.0)
+			SetDriverAggressiveness(ped, 0.0)
+			SetBlockingOfNonTemporaryEvents(ped, true)
+			SetPedConfigFlag(ped, 251, true)
+			SetPedConfigFlag(ped, 64, true)
+			SetPedStayInVehicleWhenJacked(ped, true)
+			SetPedCanBeDraggedOut(ped, false)
 		end
 		if DoesEntityExist(globalTaxi) then
 			ESX.Game.DeleteVehicle(globalTaxi)
 		end
-		
-		ESX.Game.SpawnVehicle(vehicleHash, vector3(posX, posY, posZ), heading, function(callback_vehicle)
+		--print(posX, posY, posZ, cabeza)
+		ESX.Game.SpawnVehicle(vehicleHash, vector3(posX, posY, posZ), cabeza, function(callback_vehicle)
 			platenum = math.random(1000, 9999)
 			SetVehicleNumberPlateText(callback_vehicle, "TAXI"..platenum)
 			TriggerServerEvent('shorty_slocks:breakIn', "TAXI"..platenum)
@@ -104,6 +128,7 @@ AddEventHandler('esx_aiTaxi:callTaxi', function(coords)
 			taxiBlip = true
 			globalTaxi = callback_vehicle
 			SetEntityAsMissionEntity(globalTaxi, true, true)
+			Citizen.Wait(200)
 			drive(customer.x, customer.y, customer.z, false, 'start')
 		end)
 	end
@@ -113,6 +138,15 @@ AddEventHandler('esx_aiTaxi:callTaxi', function(coords)
  end)
 	
 end)
+
+function WaitTaskToEnd(ped, task)
+	while GetScriptTaskStatus(ped, task) == 0 do -- ?
+		Wait(250)
+	end
+	while GetScriptTaskStatus(ped, task) == 1 do -- Performing the task
+		Wait(250)
+	end
+end
 
 RegisterNetEvent('esx_aiTaxi:setTaxiBlip')
 AddEventHandler('esx_aiTaxi:setTaxiBlip', function(coords)
@@ -142,69 +176,130 @@ end)
 
 Citizen.CreateThread(function()
 	while true do
-		Citizen.Wait(50)
+		Citizen.Wait(0)
 		inCar = false
-	while GetCurrentServerEndpoint() ~= ipe do
-	Citizen.Wait(1000)
-		print("Script robado a AdictosRP")
-		print("Discord: JuanFran#8945")
-		print("Si te ha llegado este script, habla con él.")
-	end
 		if customer ~= nil then
 			local vehicle = GetVehiclePedIsIn(GetPlayerPed(-1), false)
-			local matricula = GetVehicleNumberPlateText(vehicle)
-			local matricula1 = GetVehicleNumberPlateText(globalTaxi)
-			if matricula == matricula1 and matricula ~= nil and matricula1 ~= nil then
+			--local matricula = GetVehicleNumberPlateText(vehicle)
+			--local matricula1 = GetVehicleNumberPlateText(globalTaxi)
+			
+			--if matricula == matricula1 and matricula ~= nil and matricula1 ~= nil then
+			if vehicle == globalTaxi then
 				inCar = true
 				local waypoint = GetFirstBlipInfoId(8)
 				if not DoesBlipExist(waypoint) and not onTour then
-					ESX.ShowHelpNotification('¿A donde quieres ir?')
+					ESX.ShowHelpNotification('Marca en el GPS donde quieres ir')
 					Citizen.Wait(2000)
+					aparcado = true
 				else
-					tx, ty, tz = table.unpack(Citizen.InvokeNative(0xFA7C7F0AADF25D09, waypoint, Citizen.ResultAsVector()))
-					if not onTour then
-						if not targetX then
-							targetX = tx
-							targetY = ty
-							targetZ = tz
-						end
-						drive(tx, ty, tz, false, false)
-						onTour = true
+					if aparcado then
+						ESX.ShowHelpNotification('Pulsa ~INPUT_PICKUP~ para empezar el viaje')
 					end
+					if IsControlJustReleased(0, Keys['E']) then
+						aparcado = false
+						tx, ty, tz = table.unpack(Citizen.InvokeNative(0xFA7C7F0AADF25D09, waypoint, Citizen.ResultAsVector()))
+						if not onTour then
+							if not targetX then
+								targetX = tx
+								targetY = ty
+								targetZ = tz
+							end
+							drive(tx, ty, tz, false, false)
+							onTour = true
+							Citizen.Wait(15000)
+							route2 = CalculateTravelDistanceBetweenPoints(customer.x, customer.y, customer.z, targetX, targetY, targetZ)
+							exact = (route2/1000) * 110
+							price = math.floor(exact)
+							--TriggerServerEvent('esx_aiTaxi:pay', 0)
+							TriggerServerEvent('esx_aiTaxi:pay', price)
+						end
+					end
+					--if IsControlJustReleased(0, Keys['H']) then
+					--	drive(tx, ty, tz, false, 'turbo')
+					--	TriggerEvent('esx:showNotification', 'Turbo activado.')
+					--end
 				end
 			end
+		else
+			Citizen.Wait(2000)
 		end
 	end
 end)
 
 --distancechecks
 Citizen.CreateThread(function()
-	local playerPed = GetPlayerPed(-1)
 	while true do
-		Citizen.Wait(50)
+		Citizen.Wait(0)
 		if customer ~= nil then
-			myCoords = GetEntityCoords(playerPed)
+			myCoords = GetEntityCoords(GetPlayerPed(-1))
 			taxiCoords = GetEntityCoords(ped)
-			local vehicle = GetVehiclePedIsIn(playerPed, false)
-			Citizen.Wait(20)
+			local vehicle = GetVehiclePedIsIn(GetPlayerPed(-1), false)
+			--print(vehicle, globalTaxi, onTour, aparcado)
 			if vehicle == globalTaxi then
+
 				route = CalculateTravelDistanceBetweenPoints(customer.x, customer.y, customer.z, taxiCoords.x, taxiCoords.y, taxiCoords.z)
+				fuera = true
 				--check distance between me and the destination
-				if GetDistanceBetweenCoords(myCoords, targetX, targetY, targetZ) < 20 then
+				--print(GetDistanceBetweenCoords(myCoords, targetX, targetY, taxiCoords.z))
+				--TriggerEvent('esx:showNotification', 'Puedes pulsar "Control Izquierdo" 3 veces para detener el taxi')
+				--if IsControlJustReleased(0, Keys['LEFTCTRL']) then
+				--	parking(taxiCoords.x, taxiCoords.y, taxiCoords.z)
+				--	Citizen.Wait(5000)
+				--	TaskLeaveVehicle(GetPlayerPed(-1), globalTaxi, 1)
+				--	Citizen.Wait(5000)
+				--	atTarget()
+				--end
+				
+				--print("llegar", GetDistanceBetweenCoords(myCoords, targetX, targetY, myCoords.z))
+				if GetDistanceBetweenCoords(myCoords, targetX, targetY, myCoords.z) < 25 then
+					TaskVehiclePark(ped, globalTaxi,targetX, targetY, myCoords.z, 0.0, 0, 500.0, true)
+					WaitTaskToEnd(ped, 567490903)
+					--TaskVehicleDriveToCoordLongrange(ped, globalTaxi, targetX, targetY, myCoords.z, 2.0, 524863, 6.0)
+					Citizen.Wait(1000)
 					atTarget()
+					--WaitTaskToEnd(ped, 567490903)
+				end	
+			else 
+				if fuera then
+					if GetDistanceBetweenCoords(myCoords.x, myCoords.y, myCoords.z, taxiCoords.x, taxiCoords.y, myCoords.z, true) > 15 then
+						--print("salir coche")
+						atTargetFIN('cancel')
+						fuera = false
+					end
 				end
 			end
+			
+			
 			--check if taxi is next to me 
 			if customer ~= nil then
 				local distanceMeTaxi = GetDistanceBetweenCoords(customer.x, customer.y, customer.z, taxiCoords.x, taxiCoords.y, taxiCoords.z, true)
-				if distanceMeTaxi <= 30 then
+				
+				if distanceMeTaxi <= 8 then
 					if not parkingDone then
 						parking(customer.x, customer.y, customer.z)
-						TriggerEvent('esx:showNotification', 'Ya está tu taxi aqui')
+						if llegar then
+							TriggerEvent('esx:showNotification', 'Ya está tu taxi aqui')
+							llegar = false
+							
+						end	
 					end
-					if GetDistanceBetweenCoords(customer.x, customer.y, customer.z, taxiCoords.x, taxiCoords.y, taxiCoords.z, true) <= 6 then
+					if GetDistanceBetweenCoords(customer.x, customer.y, customer.z, taxiCoords.x, taxiCoords.y, taxiCoords.z, true) <= 5 then
+						if taxiArrived == false then
+							TaskVehicleDriveToCoordLongrange(ped, globalTaxi, taxiCoords.x, taxiCoords.y, taxiCoords.z, 2.0, 524863, 6.0)
+						end
 						taxiArrived = true
 					end
+				end
+			end
+		else
+			Citizen.Wait(3000)
+		end
+		if fuera then
+			if vehicle ~= globalTaxi then
+				if GetDistanceBetweenCoords(myCoords.x, myCoords.y, myCoords.z, taxiCoords.x, taxiCoords.y, myCoords.z, true) > 15 then
+					--print("salir coche")
+					atTargetFIN('cancel')
+					fuera = false
 				end
 			end
 		end
@@ -214,15 +309,27 @@ end)
 --keycontrol
 Citizen.CreateThread(function()
 	while true do
-		Citizen.Wait(50)
+		Citizen.Wait(0)
 		if customer ~= nil then
-			if taxiArrived and not inCar and not onWayBack then
-				ESX.ShowHelpNotification('Pulsa ~INPUT_PICKUP~ para entrar')
-				if IsControlJustReleased(0, Keys['E']) then
-					TaskEnterVehicle(GetPlayerPed(-1), globalTaxi, 1000, math.random(0,2), 2.0, 1, 0)
-					-- TaskWarpPedIntoVehicle(GetPlayerPed(-1), globalTaxi, math.random(0,2))
+			if taxiArrived and not inCar and not onWayBack and not fuera then
+				myCoords = GetEntityCoords(GetPlayerPed(-1))
+				taxiCoords = GetEntityCoords(ped)
+				if GetDistanceBetweenCoords(myCoords, taxiCoords.x, taxiCoords.y, taxiCoords.z) < 9 and GetDistanceBetweenCoords(myCoords, taxiCoords.x, taxiCoords.y, taxiCoords.z) > 6 then
+					WaitTaskToEnd(ped, 567490903)
+					--TaskVehicleDriveToCoordLongrange(ped, globalTaxi, taxiCoords.x, taxiCoords.y, taxiCoords.z, 2.0, 524863, 6.0)
+					--WaitTaskToEnd(ped, 567490903)
+				end
+				if GetDistanceBetweenCoords(myCoords, taxiCoords.x, taxiCoords.y, myCoords.z) < 6 then
+					ESX.ShowHelpNotification('Pulsa ~INPUT_ENTER~ para entrar')
+						
+					--if IsControlJustReleased(0, Keys['E']) then
+					--	TaskEnterVehicle(GetPlayerPed(-1), globalTaxi, 1000, math.random(0,2), 2.0, 1, 0)
+						-- TaskWarpPedIntoVehicle(GetPlayerPed(-1), globalTaxi, math.random(0,2))
+					--end
 				end
 			end
+		else
+			Citizen.Wait(3000)
 		end
 	end
 end)
@@ -232,12 +339,15 @@ Citizen.CreateThread(function()
 	while true do
 		Citizen.Wait(450)
 		if taxiBlip then
+			
 			coords = GetEntityCoords(ped)
 			TriggerEvent('esx_aiTaxi:setTaxiBlip', coords)
 			if coords == vector3(0, 0, 0)then
-			atTarget('cancel')
-			Wait(10000)
+				atTarget('cancel')
+				Wait(10000)
 			end
+		else
+			Citizen.Wait(3000)
 		end
 	end
 end)
@@ -251,6 +361,8 @@ Citizen.CreateThread(function()
 			if GetDistanceBetweenCoords(markerCoords, myCoords) > 2 then
 				DrawMarker(0, markerCoords.x, markerCoords.y, markerCoords.z+3, 0.0, 0.0, 0.0, 0, 0.0, 0.0, 3.0, 3.0, 2.0, 244, 123, 23, 100, true, true, 2, true, false, false, false)
 			end
+		else
+			Citizen.Wait(5000)
 		end
 	end
 end)
@@ -258,31 +370,67 @@ end)
 function atTarget(cancel)
 	cancelTaxi = false
 	if cancel then
-		playerPed = GetPlayerPed(-1)
-		local vehicle = GetVehiclePedIsIn(playerPed, false)
+		
+		local vehicle = GetVehiclePedIsIn(GetPlayerPed(-1), false)
 		if vehicle ~= globalTaxi then
 			TriggerEvent('esx:showNotification', 'El Taxi fue cancelado.')
 			cancelTaxi = true
+			fuera = false
 			Citizen.Wait(1000)
 			TriggerEvent('esx:showNotification', 'Espera 15 segundos antes de volver a pedir otro')
 		else
-			TriggerEvent('esx:showNotification', 'El Taxi no puede ser cancelado')
+			TriggerEvent('esx:showNotification', 'El Taxi no puede ser cancelado si vas montado en el')
+			
+			fuera = false
 			return
 		end
 	end
 	if not cancelTaxi then
 		ESX.ShowHelpNotification('Hemos llegado al destino')
-		route2 = CalculateTravelDistanceBetweenPoints(customer.x, customer.y, customer.z, targetX, targetY, targetZ)
-		exact = (route2/1000) * 110
-		price = math.floor(exact)
-		TriggerServerEvent('esx_aiTaxi:pay', price)
+		Citizen.Wait(2000)
+		TaskLeaveVehicle(GetPlayerPed(-1), globalTaxi, 1)
+		Citizen.Wait(15000)
+		
+	end
+	onWayBack = true
+	customer = nil
+	targetX = nil
+	targetY = nil
+	taxiBlip = nil
+	fuera = false
+	RemoveBlip(CarBlip)
+	parkingDone = false
+	taxiArrived = false
+	llegar = true
+	onTour = false
+	onWayBack = false
+	drive(26.92, -1736.77, 28.3, true, 'end')
+	ped = nil
+	globalTaxi = nil
+end
+
+function atTargetFIN(cancel)
+	cancelTaxi = false
+	if cancel then
+		
+		local vehicle = GetVehiclePedIsIn(GetPlayerPed(-1), false)
+		if vehicle ~= globalTaxi then
+			cancelTaxi = true
+			fuera = false
+			Citizen.Wait(1000)
+			TriggerEvent('esx:showNotification', 'El Taxi se ha ido.')
+		end
+	end
+	if not cancelTaxi then
 		TaskLeaveVehicle(GetPlayerPed(-1), globalTaxi, 1)
 		Citizen.Wait(5000)
 	end
 	onWayBack = true
 	customer = nil
 	targetX = nil
+	targetY = nil
 	taxiBlip = nil
+	fuera = false
 	RemoveBlip(CarBlip)
 	parkingDone = false
 	taxiArrived = false
@@ -294,25 +442,28 @@ function atTarget(cancel)
 end
 
 function parking(x, y ,z)
-	TaskVehiclePark(ped, globalTaxi, x, y, z, 0.0, 0, 80.0, false)
-	parkingDone = true
+	myCoords = GetEntityCoords(GetPlayerPed(-1))
+	taxiCoords = GetEntityCoords(ped)
+	if GetDistanceBetweenCoords(myCoords.x, myCoords.y, myCoords.z, taxiCoords.x, taxiCoords.y, myCoords.z, true) < 10 then
+		--print("parar")
+		TaskVehiclePark(ped, globalTaxi, x, y, z, 0.0, 0, 500.0, true)
+		parkingDone = true
+	end
 end
 
 function drive(x, y , z, delete, status)
-
-	while GetCurrentServerEndpoint() ~= ipe do
-	Citizen.Wait(1000)
-		print("Script robado a AdictosRP")
-		print("Discord: JuanFran#8945")
-		print("Si te ha llegado este script, habla con él.")
-	end
+	
+	TaskVehicleDriveToCoordLongrange(ped, globalTaxi, x, y, z, 19.5, 524863, 15.0)
+	
 	if status == 'start' then
 		Citizen.Wait(math.random(500,2000))
 		ESX.ShowHelpNotification('Un conductor está de camino.')
+		TaskVehicleDriveToCoordLongrange(ped, globalTaxi, x, y, z, 10.0, 524863, 6.0)
 	elseif status == 'end' then
 		ESX.ShowHelpNotification('Gracias por confiar en los taxistas.')
+	elseif status == 'turbo' then
+		TaskVehicleDriveToCoordLongrange(ped, globalTaxi, x, y, z, 990.0, 787260, 6.0)
 	end
-	TaskVehicleDriveToCoordLongrange(ped, globalTaxi, x, y, z, 19.5, 524863, 20.0)
 	if delete then
 		Citizen.Wait(15000)
 		DeletePed(ped)
